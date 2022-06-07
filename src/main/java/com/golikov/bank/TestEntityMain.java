331617ddc528436bfa1012219af3e166b82770a5
wwwAiru <z-a-t-o@yandex.ru>
2022-06-07 10:47:46 +0300

    maked AccountNumGenerator by temlate+User.id+ranom

--- src/main/java/com/golikov/bank/TestEntityMain.java
+++ src/main/java/com/golikov/bank/TestEntityMain.java
@@ -1,24 +1,30 @@
-//package com.golikov.bank;
-//
-//import com.golikov.bank.entity.InvestProduct;
-//import com.golikov.bank.entity.Client;
-//import com.golikov.bank.entity.ClientInvestProd;
-//
-//import java.math.BigDecimal;
-//import java.time.LocalDateTime;
-//
-//public class TestEntityMain {
-//    public static void main(String[] args) {
+package com.golikov.bank;
+
+import com.golikov.bank.entity.InvestProduct;
+import com.golikov.bank.entity.Client;
+import com.golikov.bank.entity.ClientInvestProd;
+import com.golikov.bank.utils.AccountNumGenerator;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class TestEntityMain {
+    public static void main(String[] args) {
 //        LocalDateTime createAt;
 //        LocalDateTime endDate;
 //
-//        Client client = new Client(
-//                "Первый",
-//                "Второвов",
-//                "Третьевич",
-//                "zato@yandex.ru",
-//                "pass",
-//                new BigDecimal(100000000));
+        Client client = new Client(
+                "Первый",
+                "Второвов",
+                "Третьевич",
+                "zato@yandex.ru",
+                "pass",
+                new BigDecimal(100000000));
+        client.setId(Long.valueOf(20));
+
+//        StringBuffer res = AccountNumGenerator.generate(client.getId());
+//        System.out.println(res);
+
 //        InvestProduct investProduct = new InvestProduct(
 //                "Простой вклад",
 //                "Это простой вклад под ставку 11% годовых",
@@ -34,7 +40,7 @@
 //                endDate = LocalDateTime.now());
 //        client.AddClientInvestProd(clientInvestProd);
 //        System.out.println(client.getClientInvestProds());
-//
-//
-//    }
-//}
+
+
+    }
+}
---
+++ src/main/java/com/golikov/bank/utils/AccountNumGenerator.java
@@ -0,0 +1,16 @@
+package com.golikov.bank.utils;
+
+// утилитарный класс для генерации номера счёта
+public class AccountNumGenerator {
+    private static final int SERIAL_TEMPLATE = 408777;
+    private static Long randNumber = (long) Math.floor(Math.random() * 9_000_000_000L) + 1_000_000_000L;
+
+    public static StringBuffer generate(Long clientID){
+        StringBuffer result = new StringBuffer();
+        result.append(SERIAL_TEMPLATE);
+        result.append(clientID);
+        result.append(randNumber);
+        result.setLength(16);
+        return result;
+    }
+}
