9b2c7f6363defc91648f20ec19c9d47406e1e6f0
Andrey Golikov <z-a-t-o@yandex.ru>
2022-06-07 17:41:45 +0300

    findAllByOrderByInterestRateDesc

--- src/main/java/com/golikov/bank/repository/InvestProdRepository.java
+++ src/main/java/com/golikov/bank/repository/InvestProdRepository.java
@@ -3,5 +3,9 @@ package com.golikov.bank.repository;
 import com.golikov.bank.entity.InvestProduct;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface InvestProdRepository extends CrudRepository<InvestProduct, Long> {
+    //сортировка результатов инвест продуктов по уменьшению доходности
+    List<InvestProduct> findAllByOrderByInterestRateDesc();
 }
