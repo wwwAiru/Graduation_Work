4f219a1b52879d2804fa121fd9b39202bfc16f22
Andrey Golikov <z-a-t-o@yandex.ru>
2022-06-10 00:02:50 +0300

    something

--- src/main/java/com/golikov/bank/repository/DepositAccountRepository.java
+++ src/main/java/com/golikov/bank/repository/DepositAccountRepository.java
@@ -3,5 +3,9 @@ package com.golikov.bank.repository;
 import com.golikov.bank.entity.DepositAccount;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface DepositAccountRepository extends CrudRepository<DepositAccount, Long> {
+
+    public List<DepositAccount> findDepositAccountByClientId(Long id);
 }
--- src/main/java/com/golikov/bank/service/ClientService.java
+++ src/main/java/com/golikov/bank/service/ClientService.java
@@ -1,11 +1,15 @@
 package com.golikov.bank.service;
 
+import com.golikov.bank.entity.DepositAccount;
 import com.golikov.bank.repository.ClientRepository;
+import com.golikov.bank.repository.DepositAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClientService implements UserDetailsService {
@@ -13,8 +17,15 @@ public class ClientService implements UserDetailsService {
     @Autowired
     ClientRepository clientRepository;
 
+    @Autowired
+    DepositAccountRepository depositAccountRepository;
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return clientRepository.findByEmail(email);
+    }
+
+    public List<DepositAccount> findAllClientDepoAccounts(Long id){
+        return depositAccountRepository.findDepositAccountByClientId(id);
     }
 }
