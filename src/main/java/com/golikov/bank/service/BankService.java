79d046a960f8209d163e6c2240f2a5c3200bbdbb
Andrey Golikov <z-a-t-o@yandex.ru>
2022-06-11 00:10:23 +0300

    add cardToBalance & createDepositAccount

--- src/main/java/com/golikov/bank/service/BankService.java
+++ src/main/java/com/golikov/bank/service/BankService.java
@@ -1,9 +1,12 @@
 package com.golikov.bank.service;
 
+import com.golikov.bank.entity.Client;
 import com.golikov.bank.entity.ClientTransaction;
+import com.golikov.bank.entity.DepositAccount;
 import com.golikov.bank.repository.ClientTransactionRepository;
 import com.golikov.bank.repository.ClientRepository;
 import com.golikov.bank.repository.DepositAccountRepository;
+import com.golikov.bank.utils.AccountNumGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,9 +22,19 @@ public class BankService {
     @Autowired
     ClientTransactionRepository clientTransactionRepository;
 
+    // метод для пополнения баланса клиента и запись истории транзакции
+    public void cardToBaLance(ClientTransaction clientTransaction, Client client){
+        client.setBalance(clientTransaction.getAmount());
+        clientRepository.save(client);
+        clientTransactionRepository.save(clientTransaction);
+    }
 
-    public void cardToBaLance(ClientTransaction clientTransaction){
-
+    // метод для создания инвестиционных счетов, в этом методе дополняется сущность депозит акаунта
+    // генерируется номер счёта, и устанавливается клиент владелец
+    public void createDepositAccount(Client client, DepositAccount depositAccount){
+        depositAccount.setClient(client);
+        depositAccount.setAccountNumber(AccountNumGenerator.generate(client.getId()));
+        depositAccountRepository.save(depositAccount);
     }
 
 
