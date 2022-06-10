53789d53c0bc87e95cbb286284febf0f72aa9228
Andrey Golikov <z-a-t-o@yandex.ru>
2022-06-11 00:09:19 +0300

    fix AccNumGenerator

--- src/main/java/com/golikov/bank/utils/AccountNumGenerator.java
+++ src/main/java/com/golikov/bank/utils/AccountNumGenerator.java
@@ -1,16 +1,18 @@
 package com.golikov.bank.utils;
 
+import org.springframework.context.annotation.Bean;
+
 // утилитарный класс для генерации номера счёта
 public class AccountNumGenerator {
     private static final int SERIAL_TEMPLATE = 408777;
     private static Long randNumber = (long) Math.floor(Math.random() * 9_000_000_000L) + 1_000_000_000L;
 
-    public static StringBuffer generate(Long clientID){
+    public static String generate(Long clientID){
         StringBuffer result = new StringBuffer();
         result.append(SERIAL_TEMPLATE);
         result.append(clientID);
         result.append(randNumber);
         result.setLength(16);
-        return result;
+        return result.toString();
     }
 }
