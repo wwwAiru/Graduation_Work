c12002e08943dceaa510dab3147e6784a96e1ffc
Andrey Golikov <z-a-t-o@yandex.ru>
2022-06-15 23:37:26 +0300

    component clientBalanceValidator

--- src/main/java/com/golikov/bank/validator/ClienBalanceValidator.java
+++ src/main/java/com/golikov/bank/validator/ClienBalanceValidator.java
@@ -1,18 +1,25 @@
 package com.golikov.bank.validator;
 
 import com.golikov.bank.entity.Client;
-import org.springframework.validation.BindingResult;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import java.math.BigDecimal;
 
 
+
+// кастомный валидатор проверяет на null, отрицательное значение и значение превышающее баланс клиента
+@Component
+@Getter
+@Setter
+@NoArgsConstructor
 public class ClienBalanceValidator implements Validator {
 
     private Client client;
-
-    private BindingResult bindingResult;
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -22,12 +29,14 @@ public class ClienBalanceValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         BigDecimal inputAmount = (BigDecimal) target;
-        if (client.getBalance().compareTo(inputAmount) < 0) {
-            errors.rejectValue("amount", "error.amount", "Значение не может превышать "+client.getBalance());
+        if (inputAmount == null){
+            errors.rejectValue("amount", "error.amount");
+            return;
+        } else if (client.getBalance().compareTo(inputAmount) < 0) {
+            errors.rejectValue("amount", "error.amount");
+        } else if (inputAmount.compareTo(BigDecimal.valueOf(0)) < 0) {
+            errors.rejectValue("amount", "error.amount");
             }
         }
 
-    public ClienBalanceValidator(Client client) {
-        this.client = client;
-    }
 }
