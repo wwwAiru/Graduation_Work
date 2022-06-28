0d6e319ef5d578e5e6e1314ad85806e10d2bfff4
Andrey Golikov <z-a-t-o@yandex.ru>
2022-06-28 21:31:49 +0300

    admin func: refactor search to ignore case

--- src/main/java/com/golikov/bank/domain/investment/ClientInvestProdRepository.java
+++ src/main/java/com/golikov/bank/domain/investment/ClientInvestProdRepository.java
@@ -9,9 +9,10 @@ import java.util.List;
 
 public interface ClientInvestProdRepository extends JpaRepository<ClientInvestProd, Long> {
 
-    @Query("SELECT cip FROM ClientInvestProd cip WHERE cip.account.client.email LIKE %?1% " +
-            "OR cip.account.client.firstName LIKE %?1% " +
-            "OR cip.account.client.lastName LIKE %?1% OR cip.account.client.middleName LIKE %?1% " +
-            "OR cip.investProduct.name LIKE %?1%")
+    @Query("SELECT cip FROM ClientInvestProd cip WHERE lower(cip.account.client.email) LIKE lower(concat('%', ?1,'%'))" +
+            "OR lower(cip.account.client.firstName) LIKE lower(concat('%', ?1,'%'))" +
+            "OR lower(cip.account.client.lastName) LIKE lower(concat('%', ?1,'%'))" +
+            "OR cip.account.client.middleName LIKE lower(concat('%', ?1,'%'))" +
+            "OR lower(cip.investProduct.name) LIKE lower(concat('%', ?1,'%'))")
     public List<ClientInvestProd> findAllKeyword(String keyword);
 }
