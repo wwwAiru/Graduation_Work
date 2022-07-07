93f39c6a56ed114824426ed6136b31d7ac8dabc8
user <z-a-t-o@yandex.ru>
2022-07-07 19:40:49 +0300

    bug fix

--- src/main/java/com/golikov/bank/domain/account/validator/WithdrawAccountBalanceValidator.java
+++ src/main/java/com/golikov/bank/domain/account/validator/WithdrawAccountBalanceValidator.java
@@ -2,18 +2,24 @@ package com.golikov.bank.domain.account.validator;
 
 import com.golikov.bank.config.security.UserDetailsImpl;
 import com.golikov.bank.domain.account.Account;
+import com.golikov.bank.domain.account.AccountService;
 import com.golikov.bank.domain.account.dto.WithdrawAccountBalanceFormDto;
+import com.golikov.bank.domain.client.ClientService;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Component
 @NoArgsConstructor
 public class WithdrawAccountBalanceValidator implements Validator {
+    @Autowired
+    private ClientService clientService;
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -32,11 +38,11 @@ public class WithdrawAccountBalanceValidator implements Validator {
                 .getPrincipal();
         //получаем аккаунт по id
         Account account = accountForm.getAccount();
-
+        List<Account> accounts = clientService.findClientAccounts(userDetails.getClient().getId());
         // получаем баланс аккаунта
         BigDecimal balance = account.getBalance();
 
-        if (accountForm.getAccount() == null || !userDetails.getClient().getAccounts().contains(account)) {
+        if (accountForm.getAccount() == null || !accounts.contains(account)) {
             errors.rejectValue("account", "accountWithdrawForm.account.error", "ошибка аккаунта");
         }
 
